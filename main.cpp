a50e081a00c82a06e9b4ead8f693d0f1922557fe
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-01-19 20:05:36 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-  
+#include <iostream>
+#include "Queue.h"
+
+int main() {
+    Queue<int> intQueue;
+
+    intQueue.enqueue(10);
+    intQueue.enqueue(20);
+    intQueue.enqueue(30);
+
+    while (!intQueue.isEmpty()) {
+        std::cout << intQueue.dequeue() << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
